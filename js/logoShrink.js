37ec99100aac216aaae09aaff0e0c244c5720b43
Acquil <acquil98@gmail.com>
2019-02-15 19:57:59 +0000

    Fixed bugs in mobile view

--- js/logoShrink.js
+++ js/logoShrink.js
@@ -4,11 +4,11 @@ window.onscroll = function() {
 
 function growShrinkLogo() {
     var Logo = document.getElementById("psgLogo")
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         Logo.style.height = '45px';                
         Logo.style.width = '33px';
     } else {
-        Logo.style.height = '100px';                
-        Logo.style.width = '75px';
+        Logo.style.height = '90px';                
+        Logo.style.width = '66px';
     }
 }
