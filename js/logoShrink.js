af52101bb8b1fd7e226e76baae23ec98a1dfeb26
Acquil <acquil98@gmail.com>
2019-02-20 00:03:49 +0530

    added links to organizing committee

--- js/logoShrink.js
+++ js/logoShrink.js
@@ -5,10 +5,10 @@ window.onscroll = function() {
 function growShrinkLogo() {
     var Logo = document.getElementById("psgLogo")
     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        Logo.style.height = '45px';                
+        Logo.style.height = '45px';
         Logo.style.width = '33px';
     } else {
-        Logo.style.height = '90px';                
+        Logo.style.height = '90px';
         Logo.style.width = '66px';
     }
 }
